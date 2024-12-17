a35973708e2280aacf8e30a7c05b7c4946378430
SemedEhmedov <samadra-ap116@code.edu.az>
2024-12-17 23:39:29 +0400

    n

--- Nest1/obj/Debug/net8.0/Nest1.AssemblyInfo.cs
+++ Nest1/obj/Debug/net8.0/Nest1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nest1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b07a033a5f5dbb35fb8b9f60e84c6312280320b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca50a4d0895b5690436f058cd104ec344a90bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nest1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nest1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
